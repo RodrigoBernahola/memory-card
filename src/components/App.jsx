89d48a1594808d18308bdde5a0b34349b4b66cae
Rodrigol <rodrigobernahola28@gmail.com>
2026-02-19 14:06:56 -0300

    Add fetch from API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,22 @@
 import '../styles/App.css';
 import Gameboard from './Gameboard.jsx';
 import CardContainer from './CardContainer.jsx';
-
-// const fetchResult = fetch("https://api.jikan.moe/v4/anime/21/characters")
-//                     .then(response => response.json())
-//                     .then(data =>  {
-//                       console.log(data.data[0].character.images.jpg.image_url);
-//                     });
-
-// console.log(fetchResult);
+import {useState} from 'react';
 
 function App() {
+  const [gameState, setGameState] = useState('loading');
+
+  function stateUpdate(state) {
+    setGameState(state);
+  }
+
   return (
     <>
       
       <Gameboard score={0} bestScore={0}>
 
       </Gameboard>
-      <CardContainer>
+      <CardContainer state={gameState} onStateUpdate={stateUpdate}>
 
       </CardContainer>
 
